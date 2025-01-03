f099aeaaba1e3d89a55a23359a7dcf019b6a90d4
Ritesh Singh <ravi1162001singhairtle@gmail.com>
2025-01-03 21:24:03 +0530

    fixed browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@
 import './App.css'
 import LoginForm from './pages/LoginForm.tsx'
 import Leaderboards from './pages/Leaderboards.tsx' 
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router';
+import {  Route, Routes, Link } from 'react-router';
 import Dashboard from './pages/DashBoard.tsx';
 function App() {
 
   return (
     <>
-     <Router>
             <nav>
               <div>
                 
@@ -25,7 +24,6 @@ function App() {
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/bookRoom" element={<Dashboard />} />
             </Routes>
-        </Router>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 import './index.css'
 import './Leaderboards.css'
 import App from './App'
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-     <HashRouter>
-      <App />
-    </HashRouter>
+
+        <RouterProvider router={router} />
+
+    
   </StrictMode>,
 )
