edd641437bcedfe5151a4953db9226e01df3e8bd
Ritesh Singh <ravi1162001singhairtle@gmail.com>
2025-01-19 18:36:14 +0530

    changed api url

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -50,7 +50,7 @@ const Dashboard: React.FC = () => {
             // If allotedRoom is null, fetch available rooms
             if (!response.data.allotedRoom) {
               axios
-                .get('http://hostelbackend-fd9l.onrender.com/api/v1/getAvailableRooms', {
+                .get('https://hostelbackend-fd9l.onrender.com/api/v1/getAvailableRooms', {
                   headers: {
                     Authorization: `Bearer ${token}`,
                   },
@@ -82,7 +82,7 @@ const Dashboard: React.FC = () => {
 
     const token = localStorage.getItem('access_token');
     axios
-      .get(`http://hostelbackend-fd9l.onrender.com/api/v1/getPartnerInfo?partnerId=${partnerId}`, {
+      .get(`https://hostelbackend-fd9l.onrender.com/api/v1/getPartnerInfo?partnerId=${partnerId}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -109,7 +109,7 @@ const Dashboard: React.FC = () => {
     const token = localStorage.getItem('access_token');
     axios
       .post(
-        'http://hostelbackend-fd9l.onrender.com/api/v1/bookRoom',
+        'https://hostelbackend-fd9l.onrender.com/api/v1/bookRoom',
         { roomId: selectedRoom,partnerId:partnerId },
         {
           headers: {
